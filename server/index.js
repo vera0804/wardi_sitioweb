ec3905e0dd587c96aeae32e6388728f5f647d9c7
vera0804 <vera2vn@gmail.com>
2026-06-05 10:51:10 -0600

    Cambio en formulario

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ dotenv.config({ path: join(__dirname, '..', '.env') })
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.set('trust proxy', true);
+
 app.use(cors())
 app.use(express.json())
 
